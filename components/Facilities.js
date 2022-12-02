9f295315cfda94849b37538e37be436b6671527a
Prathamesh Karambelkar <83802999+prathameshkarambelkar@users.noreply.github.com>
2022-12-03 04:23:47 +0530

    everything half done

---
+++ components/Facilities.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export const Facilities = () => {
+  return (
+    <>
+      <div>
+        <div class="relative">
+          <img
+            class=" top-0 left-0"
+            src="/img/Facbg.png"
+            alt="Workplace"
+            width="600"
+          />
+          <h3 className="absolute text-[#C9F15C] text-sm top-5 left-5">RESORT</h3>
+          <h3 className="absolute text-white text-lg underline top-11 font-bold left-5 ">FACILITIES</h3>
+
+          
+
+          <img
+            class="cursor-pointer absolute top-0 left-0  hover:shadow-outline"
+            src="/img/leaf.png"
+            width="600"
+          />
+
+        </div>
+        
+      </div>
+      
+    </>
+  );
+};
+
+export default Facilities;
---
+++ components/Footer.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="relative flex-col ">
+        <img src="/img/footer/footer1.png" />
+        <h1 className="absolute top-5 mx-12 font-extrabold text-white">Recommend your changes for us.</h1>
+        <form className="absolute top-14 left-8 flex ">
+          <input
+            type="text"
+            class="
+        form-control
+        block
+        w-full
+        px-5
+        py-1
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding
+        border border-solid border-gray-300
+        rounded
+        transition
+        ease-in-out
+        
+        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+      "
+          />
+          <button class="bg-[#197C38] text-white font-semibold py-2 px-4 mx-2 border  rounded">
+            Submit
+          </button>
+        </form>
+      </div>
+      <div className="relative">
+        <img src="/img/footer/footer2.png" />
+        <img className="absolute top-3 left-3" src="/img/WhiteLogosm.png" />
+        <h2 className="absolute top-7 text-[#FFFFFF] mx-7 my-10">The website ends here, but your journey to the remotest destinations in India and Nepal begins with Zostel. Explore with our social hostels, homestays, and luxury stays on your next holiday.</h2>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
